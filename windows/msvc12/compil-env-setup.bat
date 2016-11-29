c79995b1fe1eb75da6481fd333efc4669c2a9ecd
Nicolas Leclercq <nicolas.leclercq@synchrotron-soleil.fr>
2016-11-29 10:45:35 +0100

    reorganized runtime

--- windows/msvc12/compil-env-setup.bat
+++ windows/msvc12/compil-env-setup.bat
@@ -3,13 +3,23 @@
 ::------------------------------------------------------------------------
 :: ======== TANGO BINDING PATHS ======== 
 ::------------------------------------------------------------------------
-set BINDING_PATH=C:\mystuffs\dev\bindings\labview-bindings-github
-
+::- git clone directory
+set BINDING_PATH=C:\ica\staff\nl\labview-binding@github
+::- runtime directory
 set RUNTIME_PATH_X86=%BINDING_PATH%\runtime\windows
 set RUNTIME_PATH_X64=%BINDING_PATH%\runtime\windows
-
-set PATH=%RUNTIME_PATH_X64%\lib\vc12_dll;%PATH%
-set PATH=%RUNTIME_PATH_X86%\lib\vc10_dll;%PATH%
+::- .h
+set RUNTIME_INC_X86=%RUNTIME_PATH_X86%\include
+set RUNTIME_INC_X64=%RUNTIME_PATH_X64%\include
+::- .lib
+set RUNTIME_LIB_X86=%RUNTIME_PATH_X86%\lib\x86\tango-9.2.2-vc10
+set RUNTIME_LIB_X64=%RUNTIME_PATH_X64%\lib\x64\tango-9.2.2-vc12
+::- .dll
+set RUNTIME_BIN_X86=%RUNTIME_PATH_X86%\lib\x86\tango-9.2.2-vc10
+set RUNTIME_BIN_X64=%RUNTIME_PATH_X64%\lib\x64\tango-9.2.2-vc12
+::- path
+set PATH=%RUNTIME_BIN_X86%;%PATH%
+set PATH=%RUNTIME_BIN_X64%;%PATH%
 set PATH=%BINDING_PATH%\vis;%PATH%
 
 ::------------------------------------------------------------------------
@@ -29,56 +39,60 @@ SET LV_LIB64_LIST=labview.lib
 :: ======== YAT32 ======== 
 ::------------------------------------------------------------------------
 set YAT_ROOT32=%RUNTIME_PATH_X86%
-set YAT_INC32=%YAT_ROOT32%\include\vc10
-set YAT_LIB32=%YAT_ROOT32%\lib\vc10
-set YAT_SHARED_LIB32=%YAT_ROOT32%\lib\vc10_dll
+set YAT_INC32=%RUNTIME_INC_X86%
+set YAT_LIB32=%RUNTIME_LIB_X86%
+set YAT_BIN32=%RUNTIME_BIN_X86%
 set YAT_LIB32_LIST=yat.lib
 set YAT_DBG_LIB32_LIST=yatd.lib
+SET PATH=%YAT_BIN32%;%PATH%
 
 ::------------------------------------------------------------------------
 :: ======== YAT64 ======== 
 ::------------------------------------------------------------------------
 set YAT_ROOT64=%RUNTIME_PATH_X64%
-set YAT_INC64=%YAT_ROOT64%\include\vc12
-set YAT_LIB64=%YAT_ROOT64%\lib\vc12_dll
-set YAT_SHARED_LIB64=%YAT_ROOT64%\lib\vc12_dll
+set YAT_INC64=%RUNTIME_INC_X64%
+set YAT_LIB64=%RUNTIME_LIB_X64%
+set YAT_BIN64=%RUNTIME_BIN_X64%
 set YAT_LIB64_LIST=yat.lib
 set YAT_DBG_LIB64_LIST=yatd.lib
+SET PATH=%YAT_BIN64%;%PATH%
 
-::------------------------------------------------------------------------
+::-------------SET PATH=%LOG4TANGO_BIN64%;%PATH%-----------------------------------------------------------
 :: ======== YAT4TANGO32 ======== 
 ::------------------------------------------------------------------------
 set YAT4TANGO_ROOT32=%RUNTIME_PATH_X86%
-set YAT4TANGO_INC32=%YAT4TANGO_ROOT32%\include
-set YAT4TANGO_LIB32=%YAT4TANGO_ROOT32%\lib\vc10
-set YAT4TANGO_SHARED_LIB32=%YAT4TANGO_ROOT32%\lib\vc10_dll
+set YAT4TANGO_INC32=%RUNTIME_INC_X86%
+set YAT4TANGO_LIB32=%RUNTIME_LIB_X86%
+set YAT4TANGO_BIN32=%RUNTIME_BIN_X86%
 set YAT4TANGO_LIB32_LIST=yat4tango.lib
 set YAT4TANGO_DBG_LIB32_LIST=yat4tangod.lib
+SET PATH=%YAT4TANGO_BIN32%;%PATH%
 
 ::------------------------------------------------------------------------
 :: ======== YAT4TANGO64 ======== 
 ::------------------------------------------------------------------------
 set YAT4TANGO_ROOT64=%RUNTIME_PATH_X64%
-set YAT4TANGO_INC64=%YAT_ROOT64%\include\vc12
-set YAT4TANGO_LIB64=%YAT_ROOT64%\lib\vc12_dll
-set YAT4TANGO_SHARED_LIB64=%YAT_ROOT64%\lib\vc12_dll
+set YAT4TANGO_INC64=%RUNTIME_INC_X64%
+set YAT4TANGO_LIB64=%RUNTIME_LIB_X64%
+set YAT4TANGO_BIN64=%RUNTIME_BIN_X64%
 set YAT4TANGO_LIB64_LIST=yat4tango.lib
 set YAT4TANGO_DBG_LIB64_LIST=yat4tangod.lib
+SET PATH=%YAT4TANGO_BIN64%;%PATH%
 
 ::------------------------------------------------------------------------
 :: ======== LOG4TANGO ======== 
 ::------------------------------------------------------------------------
-SET LOG4TANGO_ROOT32=C:\mystuffs\dev\tango-9.2.2-x86-vc10
-SET LOG4TANGO_INC32=%LOG4TANGO_ROOT32%\include\vc10
-SET LOG4TANGO_LIB32=%LOG4TANGO_ROOT32%\lib\vc10_dll
-SET LOG4TANGO_BIN32=%LOG4TANGO_ROOT32%\lib\vc10_dll
+SET LOG4TANGO_ROOT32=%RUNTIME_PATH_X86%
+SET LOG4TANGO_INC32=%RUNTIME_INC_X86%
+SET LOG4TANGO_LIB32=%RUNTIME_LIB_X86%
+SET LOG4TANGO_BIN32=%RUNTIME_BIN_X86%
 SET LOG4TANGO_LIB32_LIST=log4tango.lib
 SET PATH=%LOG4TANGO_BIN32%;%PATH%
 
 SET LOG4TANGO_ROOT64=%RUNTIME_PATH_X64%
-SET LOG4TANGO_INC64=%LOG4TANGO_ROOT64%\include\vc12
-SET LOG4TANGO_LIB64=%LOG4TANGO_ROOT64%\lib\vc12_dll
-SET LOG4TANGO_BIN64=%LOG4TANGO_ROOT64%\lib\vc12_dll
+SET LOG4TANGO_INC64=%RUNTIME_INC_X64%
+SET LOG4TANGO_LIB64=%RUNTIME_LIB_X64%
+SET LOG4TANGO_BIN64=%RUNTIME_BIN_X64%
 SET LOG4TANGO_LIB64_LIST=log4tango.lib
 SET PATH=%LOG4TANGO_BIN64%;%PATH%
 
@@ -86,16 +100,16 @@ SET PATH=%LOG4TANGO_BIN64%;%PATH%
 :: ======== ZMQ ======== 
 ::------------------------------------------------------------------------
 SET ZMQ_ROOT32=%RUNTIME_PATH_X86%
-SET ZMQ_INC32=%ZMQ_ROOT32%\include\vc10
-SET ZMQ_LIB32=%ZMQ_ROOT32%\lib\vc10_dll
-SET ZMQ_BIN32=%ZMQ_ROOT32%\lib\vc10_dll
+SET ZMQ_INC32=%RUNTIME_INC_X86%
+SET ZMQ_LIB32=%RUNTIME_LIB_X86%
+SET ZMQ_BIN32=%RUNTIME_BIN_X86%
 SET ZMQ_LIB32_LIST=zmq.lib
 SET PATH=%ZMQ_BIN32%;%PATH%
 
 SET ZMQ_ROOT64=%RUNTIME_PATH_X64%
-SET ZMQ_INC64=%ZMQ_ROOT64%\include\vc12
-SET ZMQ_LIB64=%ZMQ_ROOT64%\lib\vc12_dll
-SET ZMQ_BIN64=%ZMQ_ROOT64%\lib\vc12_dll
+SET ZMQ_INC64=%RUNTIME_INC_X64%
+SET ZMQ_LIB64=%RUNTIME_LIB_X64%
+SET ZMQ_BIN64=%RUNTIME_BIN_X64%
 SET ZMQ_LIB64_LIST=zmq.lib
 SET PATH=%ZMQ_BIN64%;%PATH%
 
@@ -103,16 +117,16 @@ SET PATH=%ZMQ_BIN64%;%PATH%
 :: ======== OMNIORB ======== 
 ::------------------------------------------------------------------------
 SET OMNIORB_ROOT32=%RUNTIME_PATH_X86%
-SET OMNIORB_INC32=%OMNIORB_ROOT32%\include\vc10
-SET OMNIORB_LIB32=%OMNIORB_ROOT32%\lib\vc10_dll
-SET OMNIORB_BIN32=%OMNIORB_ROOT32%\lib\vc10_dll
+SET OMNIORB_INC32=%RUNTIME_INC_X86%
+SET OMNIORB_LIB32=%RUNTIME_LIB_X86%
+SET OMNIORB_BIN32=%RUNTIME_BIN_X86%
 SET OMNIORB_LIB32_LIST=omnithread40_rt.lib;omniORB421_rt.lib;COS421_rt.lib;omniDynamic421_rt.lib
 SET PATH=%OMNIORB_BIN32%;%PATH%
 
 SET OMNIORB_ROOT64=%RUNTIME_PATH_X64%
-SET OMNIORB_INC64=%OMNIORB_ROOT64%\include\vc12
-SET OMNIORB_LIB64=%OMNIORB_ROOT64%\lib\vc12_dll
-SET OMNIORB_BIN64=%OMNIORB_ROOT64%\lib\vc12_dll
+SET OMNIORB_INC64=%RUNTIME_INC_X64%
+SET OMNIORB_LIB64=%RUNTIME_LIB_X64%
+SET OMNIORB_BIN64=%RUNTIME_BIN_X64%
 SET OMNIORB_LIB64_LIST=omnithread40_rt.lib;omniORB421_rt.lib;COS421_rt.lib;omniDynamic421_rt.lib
 SET PATH=%OMNIORB_BIN64%;%PATH%
 
@@ -120,16 +134,16 @@ SET PATH=%OMNIORB_BIN64%;%PATH%
 :: ======== TANGO ======== 
 ::------------------------------------------------------------------------
 SET TANGO_ROOT32=%RUNTIME_PATH_X86%
-SET TANGO_INC32=%TANGO_ROOT32%\include\vc10
-SET TANGO_LIB32=%TANGO_ROOT32%\lib\vc10_dll
-SET TANGO_BIN32=%TANGO_ROOT32%\lib\vc10_dll
+SET TANGO_INC32=%RUNTIME_INC_X86%
+SET TANGO_LIB32=%RUNTIME_LIB_X86%
+SET TANGO_BIN32=%RUNTIME_BIN_X86%
 SET TANGO_LIB32_LIST=tango.lib
 SET PATH=%TANGO_BIN32%;%PATH%
 
 SET TANGO_ROOT64=%RUNTIME_PATH_X64%
-SET TANGO_INC64=%TANGO_ROOT64%\include\vc12
-SET TANGO_LIB64=%TANGO_ROOT64%\lib\vc12_dll
-SET TANGO_BIN64=%TANGO_ROOT64%\lib\vc12_dll
+SET TANGO_INC64=%RUNTIME_INC_X64%
+SET TANGO_LIB64=%RUNTIME_LIB_X64%
+SET TANGO_BIN64=%RUNTIME_BIN_X64%
 SET TANGO_LIB64_LIST=tango.lib
 SET PATH=%TANGO_BIN64%;%PATH%
 
